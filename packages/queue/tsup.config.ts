df5e80ffb0093b68cf49e107c7f82a88185f95cc
sooohka <sooohka01@gmail.com>
2025-10-12 16:56:24 +0900

    add browser support

--- packages/queue/tsup.config.ts
+++ packages/queue/tsup.config.ts
@@ -3,30 +3,30 @@ import { spawn } from "node:child_process";
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
-    return {
-        entry: ["src/index.ts"],
-        dts: true,
-        esbuildPlugins: [],
-        sourcemap: true,
-        clean: true,
-        target: "es2015",
-        // minify: true,
-        bundle: true,
-        format: ["cjs", "esm"],
-        external: ["react"],
-        async onSuccess() {
-            if (options.watch) {
-                const child = spawn("node", ["dist/index.js"]);
-                child.stdout.on("data", (data) => {
-                    console.log(data.toString());
-                });
-                child.stderr.on("data", (data) => {
-                    console.error(data.toString());
-                });
-                child.on("close", (code) => {
-                    console.log(`child process exited with code ${code}`);
-                });
-            }
-        },
-    };
+  return {
+    entry: ["src/index.ts"],
+    dts: true,
+    esbuildPlugins: [],
+    sourcemap: true,
+    clean: true,
+    target: "es2015",
+    // minify: true,
+    bundle: true,
+    format: ["cjs", "esm", "iife"],
+    external: ["react"],
+    async onSuccess() {
+      if (options.watch) {
+        const child = spawn("node", ["dist/index.js"]);
+        child.stdout.on("data", (data) => {
+          console.log(data.toString());
+        });
+        child.stderr.on("data", (data) => {
+          console.error(data.toString());
+        });
+        child.on("close", (code) => {
+          console.log(`child process exited with code ${code}`);
+        });
+      }
+    },
+  };
 });
